7a932cd5f2988f5a5a2aadb2df1c5c69d0d0abe1
LutsenkoVlad <vladlutsenko1996@gmail.com>
2019-09-29 23:09:37 +0300

    simple gRPC Aspnetcore example

---
+++ src/Clients/Clients/Services/ClientsService.cs
@@ -0,0 +1,19 @@
+﻿using Grpc.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Clients.Services
+{
+    public class ClientsService : ClientsGrpc.ClientsGrpcBase
+    {
+        public override Task<CreateClientReply> CreateClient(CreateClientRequest request, ServerCallContext context)
+        {
+            return Task.FromResult(new CreateClientReply
+            {
+                Status = "Sucess"
+            });
+        }
+    }
+}
---
+++ src/Clients/ConsoleGrpcClient/Program.cs
@@ -0,0 +1,27 @@
+﻿using Clients;
+using Grpc.Net.Client;
+using System;
+using System.Net.Http;
+
+namespace ConsoleGrpcClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var serverUrl = "https://localhost:5001";
+            var handler = new HttpClientHandler();
+
+            handler.ServerCertificateCustomValidationCallback = (message, cert, chain, errors) => true;
+
+            var httpClient = new HttpClient(handler);
+
+            var channel = GrpcChannel.ForAddress(serverUrl, new GrpcChannelOptions { HttpClient = httpClient });
+            var clientsService = new ClientsGrpc.ClientsGrpcClient(channel);
+
+            clientsService.CreateClient(new CreateClientRequest { Name = "Vlad Lutsenko" });
+
+            Console.ReadLine();
+        }
+    }
+}
